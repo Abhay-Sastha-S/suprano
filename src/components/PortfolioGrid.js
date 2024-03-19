5d020e8a3e1e0644853f64ce5603c910f323a1c8
Abhay Sastha S <abhaysastha.code@gmail.com>
2024-03-19 17:43:56 +0530

    dfghj

--- src/components/PortfolioGrid.js
+++ src/components/PortfolioGrid.js
@@ -1,70 +1,82 @@
 import '../stylesheets/PortfolioGrid.css'
 import { grid_items } from '../Items'
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 
 const PortfolioGrid = () => {
-    const selectorRefs = useRef([]);
-
     useEffect(() => {
         const selector = document.querySelectorAll(".back-prop")
         const player = document.querySelector(".video-Player")
         const close = document.querySelector(".close-icon")
-
-        selectorRefs.current = Array.from(selector);
+        const wrapper = document.querySelector(".grid-Wrapper")
 
         close.addEventListener("click", () => {
-            player.style.visibility = 'hidden'
+            player.style.opacity = "0"
+            player.style.zIndex = "-999"
             let child_el = document.querySelectorAll(".div-conf")
             child_el.forEach(el => {
                 player.removeChild(el)
             })
+            wrapper.style.visibility = wrapper.style.visibility === 'hidden' ? 'visible' : 'hidden'
         })
 
-        const handleGridItemClick = (el) => {
-            player.style.visibility = 'visible'
-            let img = el.dataset.arr.split(",")
-            img.forEach((image, index) => {
-                let cont_div = document.createElement("div")
-                player.appendChild(cont_div)
-                cont_div.classList.add("div-conf")
-                cont_div.style.backgroundImage = `url(${image})`
-                cont_div.style.setProperty("--ind", index)
-                cont_div.classList.add("anim-conf")
-                if (document.documentElement.clientWidth > 480) {
-                    if (index === (img.length - 1)) {
-                        cont_div.classList.add("hov-clc")
-                        cont_div.addEventListener("click", () => {
-                            let els = document.querySelectorAll(".div-conf")
-                            els.forEach((el, index) => {
-                                let trs = index * 28 + 14
-                                el.style.left = `${trs}%`
-                                el.style.setProperty("--lft", index === 0 ? 20 : 0)
-                                if (el.classList.contains("hov-clc")) {
-                                    el.classList.remove("hov-clc")
-                                }
-                                el.classList.add("hov-eff")
+        selector.forEach(el => {
+            el.addEventListener("click", () => {
+                player.style.zIndex = "999"
+                player.style.opacity = "1"
+                let img = el.dataset.arr.split(",")
+                img.forEach((image, index) => {
+                    let cont_div = document.createElement("div")
+                    player.appendChild(cont_div)
+                    cont_div.classList.add("div-conf")
+                    cont_div.style.backgroundImage = `url(${image})`
+                    cont_div.style.setProperty("--ind", index)
+                    cont_div.classList.add("anim-conf")
+                    if (document.documentElement.clientWidth > 480) {
+                        if (index === (img.length - 1)) {
+                            cont_div.classList.add("hov-clc")
+                            cont_div.addEventListener("click", () => {
+                                let els = document.querySelectorAll(".div-conf")
+                                els.forEach((el, index) => {
+                                    let trs = index * 28 + 14
+                                    el.style.left = `${trs}%`
+                                    el.style.setProperty("--lft", index === 0 ? 20 : 0)
+                                    if (el.classList.contains("hov-clc")) {
+                                        el.classList.remove("hov-clc")
+                                    }
+                                    el.classList.add("hov-eff")
+                                })
                             })
-                        })
+                        }
                     }
-                }
+                })
             })
-        }
-
-        selectorRefs.current.forEach(el => {
-            el.addEventListener("click", () => handleGridItemClick(el))
         })
 
         return () => {
             close.removeEventListener("click", () => {
-                player.style.visibility = 'hidden'
+                player.style.opacity = "0"
+                player.style.zIndex = "-999"
                 let child_el = document.querySelectorAll(".div-conf")
                 child_el.forEach(el => {
                     player.removeChild(el)
                 })
+                wrapper.style.visibility = wrapper.style.visibility === 'hidden' ? 'visible' : 'hidden'
             })
 
-            selectorRefs.current.forEach(el => {
-                el.removeEventListener("click", () => handleGridItemClick(el))
+            selector.forEach(el => {
+                el.removeEventListener("click", () => {
+                    player.style.zIndex = "999"
+                    player.style.opacity = "1"
+                    let img = el.dataset.arr.split(",")
+                    img.forEach((image, index) => {
+                        let cont_div = document.createElement("div")
+                        player.appendChild(cont_div)
+                        cont_div.classList.add("div-conf")
+                        cont_div.style.backgroundImage = `url(${image})`
+                        cont_div.style.setProperty("--ind", index)
+                        cont_div.classList.add("anim-conf")
+                    })
+                })
             })
         }
     }, [])
